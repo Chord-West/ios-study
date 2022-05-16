77955da57ad0c245ab65504e730f2aaeb999b534
Chord-West <gustj2005@naver.com>
2022-05-16 23:09:15 +0900

    commit "Segue Study"

---
+++ ScreenTransactionExample/ScreenTransactionExample/SeguePushViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SeguePushViewController.swift
+//  ScreenTransactionExample
+//
+//  Created by 김현서 on 2022/05/16.
+//
+
+import UIKit
+
+class SeguePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}

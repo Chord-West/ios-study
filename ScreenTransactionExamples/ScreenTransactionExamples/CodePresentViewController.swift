545bd3b0877b3b27c0d5a28b12e333af654d7f16
Chord-West <gustj2005@naver.com>
2022-05-31 00:43:41 +0900

    화면간 데이터 전달

--- ScreenTransactionExamples/ScreenTransactionExamples/CodePresentViewController.swift
+++ ScreenTransactionExamples/ScreenTransactionExamples/CodePresentViewController.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 
+protocol SendDateDelegate: AnyObject {
+    func sendData(name : String)
+}
+
 class CodePresentViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    var name:String?
+    // Delgate 패턴을 사용할 때 앞에 Weak를 붙여줘야 한다. 안 붙이면 강한 참조가 발생해 메모리 누수가 발생할 수 있다.
+    weak var delegate: SendDateDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        if let name = name{
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
     }
     
     @IBAction func tapBackButton(_ sender: UIButton) {
+        self.delegate?.sendData(name: "Gunter");
         self.presentingViewController?.dismiss(animated: true,completion: nil)
     }
 }
--- ScreenTransactionExamples/ScreenTransactionExamples/ViewController.swift
+++ ScreenTransactionExamples/ScreenTransactionExamples/ViewController.swift
@@ -7,27 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SendDateDelegate {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-    @IBAction func tapCodePushButton(_ sender: UIButton) {
-        // 스토리 보드에 있는 viewcontroller 를 인스턴스화 시켜야한다.
-        guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePushViewController")
-        else{ return}// optional로 반환하기 때문에 guard문으로 처리
-        
-        self.navigationController?.pushViewController(viewController, animated: true)
-    }
-    
+
     @IBAction func tapCodePresentButton(_ sender: UIButton) {
-        guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePresentViewController")
-        else{ return}
+        guard let viewController = self.storyboard?.instantiateViewController(identifier:
+        "CodePresentViewController") as? CodePresentViewController else{ return } // CodePresentViewController 로 다운캐스트
         viewController.modalPresentationStyle = .fullScreen // 전체화면으로 전환
+        viewController.name = "Gunter"
+        viewController.delegate = self
         self.present(viewController, animated: true, completion: nil)
         
     }
+    
+    @IBAction func tapCodePushButton(_ sender: UIButton) {
+        // 스토리 보드에 있는 viewcontroller 를 인스턴스화 시켜야한다.
+        guard let viewController = self.storyboard?.instantiateViewController(identifier:
+       "CodePushViewController") as? CodePushViewController else{ return}// optional로 반환하기 때문에 guard문으로 처리
+        viewController.name = "Gunter"
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let viewController = segue.destination as? SuguePushViewController{
+            viewController.name = "Gunter"
+        }
+    }
+    
+    func sendData(name: String){
+        self.nameLabel.text = name
+        self.nameLabel.sizeToFit()
+    }
+    
 }
 
